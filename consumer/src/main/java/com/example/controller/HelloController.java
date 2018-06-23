214e9aa04ff36206a8cb014ec38db1dd738fb099
Du Lee <du.lee@foxmail.com>
2018-06-23 14:30:33 +0800

    frist commit

---
+++ consumer/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author lidu
+ * @version 2018-06-23 01:58
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/consumer-index",method = RequestMethod.GET)
+    public String index(){
+        String producer = "PRODUCER/hello";
+        return restTemplate.getForEntity(producer,String.class).getBody();
+    }
+
+}
+
